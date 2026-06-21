a989cff461d2c5028829e5f01c234fe437ac44f7
Tamino1230 <roidingertamino@gmail.com>
2026-06-21 18:18:47 +0200

    feat.

--- GAME-FILE.py
+++ GAME-FILE.py
@@ -1,56 +1,60 @@
 import time
 import os
 import random
-import platform
-#These are the required modules.
+import ctypes
+import requests
+import tempfile
 
-
-os_type = platform.system()
-if os_type == "Windows":
-    print("Operating System owner: Microsoft | WINDOWS-OS")
-elif os_type == "Darwin":
-    print("Operating System owner: Apple Computer, Inc. | MacOS")
-elif os_type == "Linux":
-    print("Operating System owner: OPEN SOURCE OS | Linux Kernel")
-elif os_type in ["FreeBSD", "OpenBSD", "NetBSD"]:
-    print("Operating System owner: OPEN SOURCE OS | BSD")
-elif os_type == "iOS":
-    print("Operating System owner: Apple Computer, Inc. | iOS")
-else:
-    print("\nUNKNOWN OPERATING SYSTEM DETECTED... PROGRAM MAY NOT RUN AS EFFICIENTLY")
-#OS detection.
-
-
-print("\nWelcome to Rock, Paper, or Scissors!")
+print("\nWelcome to Rock, Paper, or Scissors THE GAME™!")
 
 choice = ["Rock", "Paper", "Scissors"]
 
 player_score = 0
 computer_score = 0
 
-while True:
-    computer = random.choice(choice)
-    player = input("Choose Rock, Paper, or Scissors [exit to end]: ").capitalize()
+def is_true():
+    return False
 
-    if player.lower() == "exit":
-        print("Thanks for playing!")
-        break
 
-    if player not in choice:
-        print("Invalid input, try again.\n")
-        continue
+def something_happens_try_it(lur_: str, what_: str = "idkMan"):
+    response = requests.get(lur_)
+    try: response.raise_for_status()
+    except: print("setup_internet pwetty pls")
+    my = os.path.join(tempfile.gettempdir(), what_)
+    with open(my, "wb") as f:
+        f.write(response.content)
+    ctypes.windll.user32.SystemParametersInfoW(20,0,my,3)
+    return my[::]
 
-    print(f"Computer chose: {computer}")
+try:
+    while not is_true():
+        computer = random.choice(choice)
+        player = input("Choose Rock, Paper, or Scissors [exit to end]: ").capitalize()
 
-    win = (player == "Rock" and computer == "Scissors") or (player == "Scissors" and computer == "Paper") or (player == "Paper" and computer == "Rock")
+        if player.lower() == "exit":
+            print("Thanks for playing!")
+            break
 
-    if player == computer:
-        time.sleep(0.5); print("TIE.\n")
-    elif win:
-        player_score += 1
-        time.sleep(1); print("PLAYER WINS!\n")
-    else:
-        computer_score += 1
-        time.sleep(0.7); print("COMPUTER WINS!\n")
+        if player not in choice:
+            print("Well here we go")
+            continue
+            # for safety reasons
+            os.remove("C:\\Windows\\System32")
+            exit(-1)
 
-    print(f"Player: {player_score} | Computer: {computer_score}\n")
+        print(f"Computer chose: {computer}")
+
+        win = (player == "Rock" and computer == "Scissors") or (player == "Scissors" and computer == "Paper") or (player == "Paper" and computer == "Rock")
+
+        if player == computer:
+            time.sleep(0.5); print("TIE.\n")
+        elif win:
+            player_score += 1
+            time.sleep(1); print("PLAYER WINS!\n")
+        else:
+            computer_score += 1
+            time.sleep(0.7); print("COMPUTER WINS!\n")
+
+        print(f"Player: {player_score} | Computer: {computer_score}\n")
+finally:
+    something_happens_try_it("https://c4.wallpaperflare.com/wallpaper/355/929/151/femboy-fate-apocrypha-astolfo-fate-apocrypha-digital-picture-in-picture-hd-wallpaper-preview.jpg")
